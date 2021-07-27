6452bcee566ffdf1d67bd2ae2a751266ea204709
kmekhovich <kmekhovich@ya.ru>
2021-07-27 23:57:15 +0300

    added server

--- chapter3/2/client.cpp
+++ chapter3/2/client.cpp
@@ -6,21 +6,7 @@
 #include <netinet/in.h>
 
 int main() {
-    int client_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    struct sockaddr_in sock_addr;
-    sock_addr.sin_family = AF_INET;
-    sock_addr.sin_port = htons(12345);
-    sock_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 
-    connect(client_socket, (struct sockaddr*) &sock_addr, sizeof(sock_addr));
-
-    char Buffer [] = "ping";
-    send(client_socket, Buffer, 4, MSG_NOSIGNAL);
-    recv(client_socket, Buffer, 4, MSG_NOSIGNAL);
-    shutdown(client_socket, SHUT_RDWR);
-    close(client_socket);
-
-    printf("%s\n", Buffer);
 
     return 0;
 }
